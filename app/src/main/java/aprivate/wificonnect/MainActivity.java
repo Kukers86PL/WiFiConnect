0d2fd956425a0fca13f045f487155840cb0735c1
Łukasz Grzelak <lukasz.grzelak.86@gmail.com>
2017-08-31 18:13:33 +0200

    QR scan added

--- app/src/main/java/aprivate/wificonnect/MainActivity.java
+++ app/src/main/java/aprivate/wificonnect/MainActivity.java
@@ -6,7 +6,17 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.content.Intent;
-
+import com.google.zxing.Result;
+import aprivate.wificonnect.QRBuilderParser;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.widget.Toast;
+import aprivate.wificonnect.QRBuilderParser;
+import android.content.Intent;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +34,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickBtnScan(View v)
     {
-        Button scan_button = (Button)findViewById(R.id.buttonScan);
-        scan_button.setText("Scan QR Clicked");
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            String QR_text = result.getContents();
+            QRBuilderParser builderParses = new QRBuilderParser();
+            String SSID = builderParses.parseSSID(QR_text);
+            String Pass = builderParses.parsePass(QR_text);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
