e58265634864327bc980afe751017c17ac0645a2
Łukasz Grzelak <lukasz.grzelak.86@gmail.com>
2017-08-31 14:22:41 +0200

    Passwor prompt added

--- app/src/main/java/aprivate/wificonnect/WiFiListActivity.java
+++ app/src/main/java/aprivate/wificonnect/WiFiListActivity.java
@@ -1,7 +1,9 @@
 package aprivate.wificonnect;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,10 +18,16 @@ import android.widget.ArrayAdapter;
 import java.util.HashMap;
 import android.widget.AdapterView;
 import aprivate.wificonnect.QRBuilderParser;
+import android.widget.EditText;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.content.DialogInterface;
 
 public class WiFiListActivity extends AppCompatActivity {
 
     private List<WifiConfiguration> configs;
+    private String SSID = "";
+    private String Pass = "";
 
     private class StableArrayAdapter extends ArrayAdapter<String> {
 
@@ -44,6 +52,34 @@ public class WiFiListActivity extends AppCompatActivity {
             return true;
         }
 
+    }
+
+    private void getPass()
+    {
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        final EditText input = new EditText(this);
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        input.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        input.setId(R.id.dialog_edit_text);
+
+        alert.setView(input);
+        alert.setTitle("Password Required");
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                    EditText edit = (EditText) ((AlertDialog) dialog).findViewById(R.id.dialog_edit_text);
+                    Pass = edit.getText().toString();
+
+                    QRBuilderParser builderParser = new QRBuilderParser();
+                    String QR_string = builderParser.buildQR(SSID, Pass);
+                    String SSID = builderParser.parseSSID(QR_string);
+                    String Pass = builderParser.parsePass(QR_string);
+
+                    dialog.cancel();
+                }
+            });
+        alert.show();
     }
 
     @Override
@@ -78,19 +114,12 @@ public class WiFiListActivity extends AppCompatActivity {
                     if (configs != null) {
                         final String item = (String) parent.getItemAtPosition(position);
 
-                        int found = -1;
                         for (int i = 0; i < configs.size(); i++) {
                             if (configs.get(i).SSID == item) {
-                                found = i;
+                                SSID = item;
+                                getPass();
                                 break;
                             }
-                        }
-
-                        if (found != -1) {
-                            QRBuilderParser builderParser = new QRBuilderParser();
-                            String QR_string = builderParser.buildQR(configs.get(found).SSID, "test");
-                            String SSID = builderParser.parseSSID(QR_string);
-                            String Pass = builderParser.parsePass(QR_string);
                         }
                     }
                 }
